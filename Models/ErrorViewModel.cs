b42c36942d5a1bebd80623249b86f3d68eca7a7a
loren <loren@LORENZO>
2024-04-07 17:10:12 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mist452FinalProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
