14b60641606bf0301ca54856ba58c455ad0ba355
Jacob Pauley <jjp00004@mix.wvu.edu>
2024-04-09 17:24:13 -0400

    Created game model

---
+++ Models/Game.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace Mist452FinalProject.Models
+{
+    public class Game
+    {
+        public int GameID { get; set; }
+
+        [DisplayName("Game Date: ")]
+
+        public string opponent { get; set; }
+
+        public string score { get; set; }
+
+        public int posessionStat { get; set; }
+
+        public int shotsStat { get; set; }
+        public int SavesStat { get; set; }
+        public int foulsStat { get; set; }
+        public int filmURL { get; set; }
+    }
+}
