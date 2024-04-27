5302476f6f9466a2277340bf664cfb430e46ecdb
Toadops <142448988+Toadops@users.noreply.github.com>
2024-04-26 20:41:10 -0400

    commiting

--- Areas/Coach/Controllers/GameController.cs
+++ Areas/Coach/Controllers/GameController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Mist452FinalProject.Data;
 using Mist452FinalProject.Models;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 namespace Mist452FinalProject.Areas.Coach.Controllers
 {
     [Area("Coach")]
+    
     public class GameController : Controller
     {
         private readonly ProjectDBContext _dbContext;
